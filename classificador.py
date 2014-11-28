479d17dfb4219a073768f95ed19c851550aff8fd
David Przybilla <dav.alejandro@gmail.com>
2014-11-28 20:22:33 +0000

    agregando PCA

--- classificador.py
+++ classificador.py
@@ -3,45 +3,49 @@ import numpy as np
 import json
 from sklearn.svm import SVC
 from nltk.corpus import stopwords
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
+
+import numpy as np
+import matplotlib as mlp
+import matplotlib.pyplot as plt
+from sklearn import decomposition
 
 class Classifier:
 
-    def __init__(self, texts, labels):
+    def __init__(self, texts, labels, weights):
         self.texts = texts
-        self.classifier =  SVC()
+        self.classifier = linear_model.LogisticRegression()
 
         # Vectorize
-        # mid_df debe ser 2 ;p
-        self.vectorizer = CountVectorizer(ngram_range=(1, 1), min_df=1, max_df=0.8,
+        self.vectorizer = CountVectorizer(ngram_range=(1, 1), min_df=0.0, max_df=0.9,
                                   strip_accents='unicode', stop_words=stopwords.words('spanish'), binary=False)
         self.vectorizer.fit(texts)
-        
-        # Normalize
-        self.tf_transformer = TfidfTransformer(use_idf=False)
         word_counts = self.vectorizer.transform(texts)
+        # Normalize
+        self.tf_transformer = TfidfTransformer(use_idf=True)
         data = self.tf_transformer.fit_transform(word_counts)
 
+        n_eigenfaces = 200
+        self.pca = decomposition.RandomizedPCA(n_components=n_eigenfaces, whiten=True)
+        pca_features = self.pca.fit_transform(data.toarray() )
 
-        self.classifier.fit(data, labels)
+        with plt.style.context('cev plot'):
+            # revisando que el numero de eigenfaces tenga sentido..
+            plt.figure(figsize=(8, 6))
+            plt.title('cev vs eigenFace')
+            plt.plot(self.pca.explained_variance_ratio_.cumsum())
+            #plt.show()
+
+        self.classifier.fit(pca_features, labels)
 
     def get_vector(self, text):
         vector = self.vectorizer.transform([text])
-        normalized_vector = self.tf_transformer.fit_transform(vector)
-        return normalized_vector
+        normalized_vector = self.tf_transformer.transform(vector)
+        vector = self.pca.transform(vector.toarray() )
+        return vector
 
     def classify(self, text):
         vector = self.get_vector(text)
-        print(self.classifier.predict(vector))
-
-
-
-texts = [
-"El primero de ellos es básicamente un control parental bastante personalizable porque permite que los peques usen nuestros equipos sin meterse donde no deben ya que permite escoger e identificar por colores las aplicaciones que ellos pueden utilizar o, en todo caso, ocultarlas cuando el terminal esté en sus manos. En cuanto a Rooms, es básicamente una administración para compartir contenido por grupos, podemos seleccionar ciertos contactos con los que sólo queremos compartir imágenes y otro, por ejemplo, para el trabajo; cada grupo tiene un chat y la posibilidad de compartir imágenes, calendario y más.",
-"En cuanto a DataSense, que no es una aplicación que primera vez llegamos a escuchar, vamos a poder tener el control de nuestros datos de navegación, incluso con estadísticas por cada aplicación. La pega está en que en algunos casos será necesario que nuestro operador lo permita."
-]
-
-labels = [0, 1]
-
-c =Classifier(texts, labels)
-c.classify("este es un texto acerca de app aplicaciones , pequeños")
+        print("%s : %s"%(self.classifier.predict(vector), text))
 
